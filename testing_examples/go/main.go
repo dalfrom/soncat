fbee62bbb5a98c2f90f0772151ccd884b2457b78
Mirko Dal Fara <mirko.dalfarra@gmail.com>
2024-12-25 17:27:57 +0100

    Updating the Go testing package

--- testing_examples/go/main.go
+++ testing_examples/go/main.go
@@ -20,29 +20,59 @@ func main() {
 	defer conn.Close()
 	fmt.Println("Connected to server!")
 
+	// Send a single message
+	if sendMessages(conn, 1) != nil {
+		fmt.Println("Error sending messages.")
+		return
+	}
 	// Send binary data
-	message := []byte(`{ "data": { "app_name": "go_app" } }`)
-	fmt.Printf("Sending data: %v\n", message)
+	// message := []byte(`{ "data": { "app_name": "go_app" } }`)
+	// fmt.Printf("Sending data: %v\n", message)
 
-	_, err = conn.Write(message)
-	if err != nil {
-		fmt.Printf("Error sending data: %v\n", err)
-		return
+	// _, err = conn.Write(message)
+	// if err != nil {
+	// 	fmt.Printf("Error sending data: %v\n", err)
+	// 	return
+	// }
+	// fmt.Println("Data sent!")
+
+	// // Wait for a response (if the server echoes back data)
+	// buffer := make([]byte, 1024)
+	// n, err := conn.Read(buffer)
+	// if err != nil {
+	// 	fmt.Printf("Error reading response: %v\n", err)
+	// 	return
+	// }
+
+	// fmt.Printf("Received response: %v\n", buffer[:n])
+
+	// // Simulate keeping the connection alive for a short duration
+	// fmt.Println("Keeping connection alive for a few seconds...")
+	// time.Sleep(1 * time.Second)
+	// fmt.Println("Closing connection.")
+}
+
+func sendMessages(conn net.Conn, messageCount int) error {
+	for i := 0; i < messageCount; i++ {
+		sendingTime := time.Now().String()
+		message := []byte(fmt.Sprintf(`{ "data": {
+			"type": "info",
+			"sent_at": "%s",
+			"service": "go_app",
+			"payload": "Hello from Go!",
+			"p_type": "string"
+		} }`, sendingTime))
+		fmt.Printf("Sending message %d\n", i)
+
+		_, err := conn.Write(message)
+		if err != nil {
+			fmt.Printf("Error sending message %d: %v\n", i, err)
+			return err
+		}
+		fmt.Printf("Message %d sent!\n", i)
+
+		time.Sleep(1 * time.Second / 100)
 	}
-	fmt.Println("Data sent!")
 
-	// Wait for a response (if the server echoes back data)
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Printf("Error reading response: %v\n", err)
-		return
-	}
-
-	fmt.Printf("Received response: %v\n", buffer[:n])
-
-	// Simulate keeping the connection alive for a short duration
-	fmt.Println("Keeping connection alive for a few seconds...")
-	time.Sleep(5 * time.Second)
-	fmt.Println("Closing connection.")
+	return nil
 }
